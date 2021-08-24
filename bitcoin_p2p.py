33400e5d7be6dda21f7b9dda93c3a17de3128ba6
rishiA <rishiapril2000@gmail.com>
2021-08-24 23:23:55 +0530

    added visualization of blockchain

--- bitcoin_p2p.py
+++ bitcoin_p2p.py
@@ -1,6 +1,9 @@
 import numpy as np
 from queue import PriorityQueue
 import uuid
+import networkx as nx
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 class PriorityEntry(object):
 
@@ -45,13 +48,14 @@ class Transaction:
     def print(self):
         print(f"idx: {self.idx}, idy: {self.idy}, c: {self.c}")
 class Block:
-    def __init__(self, parent, txns, balances, length=1):
+    def __init__(self, parent, txns, balances, miner, length=1):
         self.id = str(uuid.uuid4())
         self.parent = parent
         self.txns = txns
         self.balances = balances
         self.num_txns = len(txns)
         self.length = length
+        self.miner = miner
     def contains(self, txn):
         if self.parent is None:
             return txn in self.txns.keys()
@@ -69,7 +73,7 @@ class Simulator:
         self.z = z
         self.Ttx = Ttx
         self.Tks = Tks
-        self.genesis_block = Block(None, {}, {i:0 for i in range(self.n)})
+        self.genesis_block = Block(None, {}, {i:0 for i in range(self.n)}, None)
         self.block_chain = {self.genesis_block.id:self.genesis_block}
         num_slow = int(round(n * z))
         self.nodes = [node(i, i<=num_slow, self.genesis_block, Tk=Tks[i]) for i in range(n)]
@@ -79,7 +83,7 @@ class Simulator:
         self.event_space = {"gen_block": self.gen_block, "send_block": self.send_block, "receive_block": self.receive_block, "gen_transaction": self.gen_transaction, "receive_transaction": self.receive_transaction}
         self.time = 0
         self.MAX_TIME = 100
-        self.MAX_NUM_EVENTS = 1000
+        self.MAX_NUM_EVENTS = 5000
         self.MAX_NUM_TRANSACTIONS = 1000
         self.MINING_REWARD = 50
         self.eps = 1e-7
@@ -178,7 +182,7 @@ class Simulator:
         new_balances[i] += self.MINING_REWARD
         self.txns[coinbase_txn.id] = coinbase_txn
         txns[coinbase_txn.id] = True
-        new_block = Block(node.longest_chain_block, txns, new_balances, node.longest_chain_block.length+1)
+        new_block = Block(node.longest_chain_block, txns, new_balances, node, node.longest_chain_block.length+1)
         new_time = time + node.get_pow_delay()
         node.send_block = True
         new_params = i, new_block.id, time
@@ -242,8 +246,68 @@ class Simulator:
         return True
     def get_inter_arrival(self):
         return 20 #np.random.exponential(self.Ttx)
+    def find_confirm_chain(self):
+        node_wise_last_blocks_temp = []
+        min_length = len(self.block_chain)
+        for node in self.nodes:
+            node_wise_last_blocks_temp.append(node.longest_chain_block)
+            min_length = min(node.longest_chain_block.length, min_length)
+        node_wise_last_blocks = []
+        for block in node_wise_last_blocks_temp:
+            cur_block = block
+            while cur_block.length > min_length:
+                cur_block = cur_block.parent
+            node_wise_last_blocks.append(cur_block)
+        backtrack = True
+        while backtrack:
+            node_wise_last_blocks_temp = node_wise_last_blocks
+            node_wise_last_blocks = []
+            if not all(block.id == node_wise_last_blocks_temp[0].id for block in node_wise_last_blocks_temp):
+                backtrack = True
+                for block in node_wise_last_blocks_temp:
+                    cur_block = block.parent
+                node_wise_last_blocks.append(cur_block)
+            else:
+                backtrack = False
+                confirm_block = node_wise_last_blocks_temp[0]
+        return confirm_block
+    def show_blockchain(self):
+        confirm_block = self.find_confirm_chain()
+        tree = nx.DiGraph()
+        for block_id, block in self.block_chain.items():
+            tree.add_node(block_id, status='unconfirmed')
+        for block_id, block in self.block_chain.items():
+            if block.parent is not None:
+                tree.add_edge(block_id, block.parent.id)   
+        cur_block = confirm_block
+        while True:
+            tree.nodes[cur_block.id]['status'] = 'confirmed'
+            if cur_block.parent is not None:
+                cur_block = cur_block.parent
+            else:
+                break
+        tree.nodes[self.genesis_block.id]['status'] = 'genesis'
+        node_color = []
+        genesis_color, confirmed_color, unconfirmed_color = 'blue', 'red', 'yellow'
+        for node in tree.nodes(data=True):
+            if 'unconfirmed' == node[1]['status']:
+                node_color.append(unconfirmed_color)
+            elif 'confirmed' == node[1]['status']:
+                node_color.append(confirmed_color)
+            elif 'genesis' == node[1]['status']:
+                node_color.append(genesis_color)
+
+        nx.draw_networkx(tree, with_labels=False, node_size=10, node_color=node_color, width=0.5, arrowsize=5)
+        genesis_patch = mpatches.Patch(color=genesis_color, label='Genesis block')
+        confirmed_patch = mpatches.Patch(color=confirmed_color, label='Confirmed block')
+        unconfirmed_patch = mpatches.Patch(color=unconfirmed_color, label='Unconfirmed block')
+        plt.legend(handles=[genesis_patch, confirmed_patch, unconfirmed_patch], loc="upper right")        
+        plt.savefig('blockchain.png', dpi=300, bbox_inches='tight')
+        plt.show()
+
 if __name__ == '__main__':
     Tks = [100, 90, 80, 70, 60, 55, 50, 45, 40, 30]
     np.random.shuffle(Tks)
     sim = Simulator(10, 0, 10, Tks)
     sim.simulate()
+    sim.show_blockchain()
